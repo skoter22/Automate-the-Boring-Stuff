e3c787b52d1d56e1a26675a889988717397f4494
skoter22 <sbhorner@gmail.com>
2019-01-07 22:54:16 -0500

    Add files via upload

---
+++ vampire.py
@@ -0,0 +1,8 @@
+if name == 'Alice':
+    print ('Hi Alice')
+elif age < 12:
+    print ('You are not Alice, kido.')
+elif age > 100:
+    print  ('Your are not Alice, grannie.')
+elif age > 2000:
+    print ('Unlike you, Alice is not an undeard, immortal vampire.')
